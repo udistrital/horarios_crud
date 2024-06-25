b2d6c04ebc7e01ac885eebb4618e079f09c87d4c
newkrux <newkrux@gmail.com>
2024-06-25 11:24:09 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-export HORARIOS_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/horarios_crud/db/username --output text --query Parameter.Value)"
-export HORARIOS_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/horarios_crud/db/password --output text --query Parameter.Value)"
+export HORARIOS_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/horarios_mongo_crud/db/username --output text --query Parameter.Value)"
+export HORARIOS_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/horarios_mongo_crud/db/password --output text --query Parameter.Value)"
 
 exec node dist/main
