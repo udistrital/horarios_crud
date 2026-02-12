769850ea887054482edfe505c35bc71c59293f3c
Stiven Osorio <jsosoriov@udistrital.edu.co>
2026-02-12 14:54:19 -0500

    fix: Limpieza de drone
    Se eliminan comenarios en el archivo drone.yml y se deja por defecto el valor de producción para RUNMODE.

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,6 +6,6 @@ export const environment = {
     DB: process.env.HORARIOS_CRUD_DB,
     HTTP_PORT: process.env.HORARIOS_CRUD_HTTP_PORT,
     AUTH_DB: process.env.HORARIOS_CRUD_AUTH_DB,
-    RUNMODE: process.env.HORARIOS_CRUD_RUNMODE,
+    RUNMODE: process.env.HORARIOS_CRUD_RUNMODE||'prod',
     PARAMETER_STORE: process.env.PARAMETER_STORE
 };
